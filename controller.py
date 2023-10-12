18e4cddb33fb814c784b6159f95787a9953a30c6
unknown <richardhickson.nz@gmail.com>
2023-10-12 18:28:35 +1300

    da

--- controller.py
+++ controller.py
@@ -1,24 +1,6 @@
 from scraper import *
 from removeChoices import removeChoices
 
-'''def main_controller_no_statty(collection, grade, remove):
-    weapon_grades = ['Consumer', 'Industrial', 'Mil-Spec', 'Restricted', 'Classified', 'Covert']
-
-    going_for = get_weapons_from_coll(collection, weapon_grades[weapon_grades.index(grade) + 1])
-    desired = 100
-
-    for j in remove:
-        going_for.remove(j)
-
-    for i in going_for:
-        temp = calculate_float(i, 0.07)
-        if temp < desired:
-            desired = temp
-    print(desired)
-    results, links = scrape(collection, grade, desired)
-    ev = ev_calc(results['x_sum'], results['y_mean'], collection, grade)'''
-
-
 def main_controller(collection, grade, remove, statty = False):
     weapon_grades = ['Consumer', 'Industrial', 'Mil-Spec', 'Restricted', 'Classified', 'Covert']
 
@@ -62,8 +44,6 @@ statty = False
 
 #results = main_controller(collection, grade, remove, statty=statty)
 
-#main_controller_no_statty(collection, grade, remove)
-#main_controller_statty(collection, grade, remove)
 
 new_float = 0.07
 #adjust_controller(collection, grade, new_float, statty=statty)
